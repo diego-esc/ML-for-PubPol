be539e1a892f95e39ef0d858a1f1830dd131f817
diego-esc <46636532+diego-esc@users.noreply.github.com>
2019-04-09 10:58:54 -0500

    Add files via upload

--- HW1.py
+++ HW1.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import censusgeocode as cg
 import numpy as np
+from scipy import stats
 
 # Problem 1.1: Data
 # Download data using a data token/key:
@@ -32,24 +33,24 @@ sns.heatmap(cross_area)
 plt.show()
 
 
-
-
-
-
-df_locations = df_crime[(df_crime.longitude.isnull()==False) & (df_crime.latitude.isnull()==False)]
-for obs in df_locations.index:
-	print(obs)
-	new = cg.coordinates(x=df_locations.longitude[obs],y=df_locations.latitude[obs])
-cg.coordinates(x=df_crime.longitude,y=df_crime.latitude)
-
+# Merge with geographical data:
+df_locations = df_crime[(df_crime.longitude.isnull()==False) & 
+	(df_crime.latitude.isnull()==False)]
 df_locations = df_crime[(df_crime.longitude.isnull()==False) & 
 (df_crime.latitude.isnull()==False)]
-cg.coordinates(x=df_locations.longitude,y=df_locations.latitude)
 
+df_locations['address'] = df_locations['block'].str.replace('XX','01')
+df_locations['city'] = 'Chicago'
+df_locations['state'] = 'IL'
+df_locations = df_locations[['address', 'city', 'state']]
+df_locations = df_locations.sample(frac=0.1, random_state=1)
+df_locations = df_locations.reset_index()
+df_locations.index.names = ['id'] # This is the format required by the geocoder
+df_locations.drop('index', axis=1, inplace=True)
 
+df_locations.to_csv('csv_locations.csv')
 
-
-
+results = censusbatchgeocoder.geocode("csv_locations.csv", zipcode=None)
 
 # Part 2:
 url_acs = "https://api.census.gov/data/2017/acs/acs5?"
@@ -81,7 +82,6 @@ df_acs['prop_male'] = df_acs.B01001_002E/df_acs.B01001_001E
 df_acs['income'] = df_acs.B19001_001E
 
 # Problem 3:
-
 df_crime['date'] = pd.to_datetime(df_crime['date'])
 df_crime['period'] = np.where((df_crime['date'] >= '2017-6-28 00:00:00') & 
 	(df_crime['date'] < '2017-7-28 00:00:00'), "pre", None)
@@ -100,28 +100,21 @@ def find_block(address):
 # a:
 address = '2111 S Michigan Ave'
 tract, block, block_group = find_block(address)
-df_michigan = df_crime[df_crime['block']==block]
-df_michigan.value_counts()
+df_michigan = df_crime[df_crime['community_area']=='33']
+df_michigan['primary_type'].value_counts()
 
 # b:
 address_uptown = '4800 N Winthrop Ave'
 address_garfield = '5 N Kedzie Ave'
 tract1, block1, block_group1 = find_block(address_uptown)
 tract2, block2, block_group2 = find_block(address_garfield)
-df_theft = df_theft[df_theft['block']==block1 or df_theft['block']==block2]
-cross_year = pd.crosstab(df_theft['primary_type'], df_theft['block'])
+df_theft = df_crime[(df_crime['community_area']=='27') | 
+	(df_crime['community_area']=='6')]
+cross_area = pd.crosstab(df_theft['primary_type'], df_theft['community_area'])
 
 # c:
 # Using bayes rule we get:
 # P(G|B)=P(B|G)*P(G)/(P(B|G)*P(G)+P(B|U)*P(U))
 p_g_b = (100/600)*(600/1000)/((100/600)*(600/1000)+(160/400)*(400/1000))
 
-
--- 
-
-Diego Escobar Salce
-Ph. D. Student | Class of 2022
-The University of Chicago Harris School of Public Policy
-
-1.312.678.5684 | descobarsalce@uchicago.edu
 
